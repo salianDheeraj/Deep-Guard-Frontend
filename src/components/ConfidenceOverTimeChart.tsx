73181eb6d12127b326b9233d322ec833f618c068
Anurag <anurag754926@gmail.com>
2025-11-09 13:25:13 +0530

    hooks added

--- src/components/ConfidenceOverTimeChart.tsx
+++ src/components/ConfidenceOverTimeChart.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useMemo } from 'react';
+import { useChartAnimation } from '@/hooks/useChartAnimation';
 
 interface ConfidenceOverTimeChartProps {
   frameWiseConfidences: number[];
